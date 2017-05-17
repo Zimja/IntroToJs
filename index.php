550d7cd6ec1f3ecca6eb10b205998f14cce3eaa9
Brian Saylor <bsaylor3@mail.greenriver.edu>
2017-05-16 19:00:24 -0500

    added files to repository

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    /*
+    Author: Brian Saylor
+    05/15/2017
+    handles routing using fat free framework*/
+    
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+    session_start();
+    
+    //Create an instance of the Base class
+    $f3 = Base::instance();
+    
+    $f3->set('DEBUG', 3);
+    
+    //Define a default route
+    $f3->route('GET /', function() {
+                $view = new View;
+                echo $view->render
+                    ('intro.js');
+            });
+    
+    //Run fat free
+    $f3->run();
+?>
